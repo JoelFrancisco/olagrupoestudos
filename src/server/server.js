48d805a7949facc36493b8889df87dc807097c9c
JoelFrancisco <joelfranciscofilho@gmail.com>
2023-06-10 15:48:37 -0300

    Adicionando endpoint de health check

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,9 @@
-console.log("ola pelo server")
+import express from "express";
+
+const app = express();
+
+app.get("/health", (_, res) => {
+  res.status(200).json({ message: "ok" })
+})
+
+app.listen(3000);
