c2970f4c6995bed45b4904e1e39c432be57df780
Joel Francisco <joelfranciscofilho@gmail.com>
2023-06-10 19:28:01 +0000

    endpoint login

--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (_, res) => {
   res.status(200).json({ message: "ok" })
+})
+
+app.post("/login", (req, res) => {
+  const body = req.body;
+
+  // query no banco top
+  const user = querynobanco();
+
+  if (bycript.compare(user.password, body.password) && user.name === body.username) {
+    return res.status(200).json({ message: "ok" });
+  } else {
+    return res.status(401).json({ message: "BANIDO" });
+  }
 })
 
 app.listen(3000);
